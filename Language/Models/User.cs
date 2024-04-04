6c69a6de6b0a631e8dc83b458bdc7e15805eb9d8
masar4073 <mbahgenius123@gmail.com>
2024-04-04 14:48:53 +0700

    Add project files.

---
+++ Language/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Language.Models
+{
+    public class User
+    {
+        public int user_id { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string address { get; set; }
+        public string phone_number { get; set; }
+
+    }
+}
