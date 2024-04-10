071f6b25c36873cbfb9e05485d4bc40ca7d7f1ed
masar4073 <mbahgenius123@gmail.com>
2024-04-10 14:32:52 +0700

    add DTO & Models for Payment Method

---
+++ Language/DTOs/Payment_Method/Payment.cs
@@ -0,0 +1,9 @@
+﻿namespace Language.DTOs.Payment_Method
+{
+    public class Payment
+    {
+        public string payment_name {  get; set; }
+        public string payment_status { get; set;}
+        public string payment_description { get; set;}
+    }
+}
---
+++ Language/Models/Payment.cs
@@ -0,0 +1,10 @@
+﻿namespace Language.Models
+{
+    public class Payment
+    {
+        public Guid id_payment_method {  get; set; }
+        public string payment_name { get; set; }
+        public string payment_status { get; set; }
+        public string payment_description { get; set; }
+    }
+}
