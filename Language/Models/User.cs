0508561050452a5a10f3db819f9e5183c691b44d
masar4073 <mbahgenius123@gmail.com>
2024-04-12 08:58:37 +0700

    add DTO & Models for Payment_Method

--- Language/Models/User.cs
+++ Language/Models/User.cs
@@ -9,5 +9,10 @@ namespace Language.Models
         public string address { get; set; }
         public string phone_number { get; set; }
 
+        public ICollection<Checkout> checkout { get;}
+        public ICollection<User> users { get;}
+
+
+
     }
 }
