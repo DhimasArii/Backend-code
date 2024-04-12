71ccf0ee2239c4c3c3511e84b42c0320e4665637
DhimasArii <dhimasari03@gmail.com>
2024-04-13 06:24:51 +0700

    Category_API

---
+++ Language/DTOs/Category/CategoryDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Language.DTOs.Category
+{
+    public class CategoryDTO
+    {
+        public string category_name {  get; set; }
+        public string category_description { get; set;}
+    }
+}
---
+++ Language/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Language.Models
+{
+    public class Category
+    {
+        public Guid category_id { get; set; }
+        public string category_name { get; set;}
+        public string category_description { get; set;}
+    }
+}
