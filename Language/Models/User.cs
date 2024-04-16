f9af08b0e1011f6ade8e67efaa10ca6e6014db02
Ari Wibowo <48719338+masar4073@users.noreply.github.com>
2024-04-16 16:34:28 +0700

    add try catch for connection

--- Language/Models/User.cs
+++ Language/Models/User.cs
@@ -5,6 +5,8 @@
         public Guid user_id { get; set; }
         public string email { get; set; }
         public string passwords { get; set; }
+        public string address { get; set; } = string.Empty;
+        public string phone_number { get; set; } = string.Empty ;
        
 
     }
