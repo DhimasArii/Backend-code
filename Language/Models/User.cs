99737e77a5359ee1fabc0e0c14666d50b03fb0d9
DhimasArii <dhimasari03@gmail.com>
2024-04-18 17:46:47 +0700

    new update email verification

--- Language/Models/User.cs
+++ Language/Models/User.cs
@@ -5,13 +5,7 @@
         public Guid user_id { get; set; }
         public string email { get; set; }
         public string passwords { get; set; }
-<<<<<<< HEAD
         public bool IsActivated { get; set; }
-=======
-        public string address { get; set; } = string.Empty;
-        public string phone_number { get; set; } = string.Empty ;
-       
->>>>>>> refs/remotes/origin/master_new
 
     }
 }
