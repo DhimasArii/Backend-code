42d066179a2fefafed42ed11531a99c29a59a481
DhimasArii <dhimasari03@gmail.com>
2024-04-26 16:17:02 +0700

    My_class

---
+++ Language/Controllers/MyClassController.cs
@@ -0,0 +1,122 @@
+﻿using Language.Data;
+using Language.DTOs.MyClass;
+using Language.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Language.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyClassController : ControllerBase
+    {
+        private readonly MyClassData _myclass;
+
+        public MyClassController(MyClassData myClassData)
+        {
+            _myclass = myClassData;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                List<My_Class> my_Classes = _myclass.GetAll();
+                return Ok(my_Classes);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("GetAllByMyClassId")]
+
+        public IActionResult GetAllByMyClassId(Guid class_id) 
+        {
+            try
+            {
+                List<My_Class> my_Classes = _myclass.GetAllByClass_id(class_id);
+                return Ok(my_Classes);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost("CreateMyClass")]
+        public IActionResult CreateMyClass([FromBody] MyClassDTO myClassDto)
+        {
+            if (myClassDto == null)
+            {
+                return BadRequest("Data should be inputted.");
+            }
+
+            My_Class myClass = new My_Class
+            {
+                class_id = Guid.NewGuid(),
+                user_id = myClassDto.user_id,
+                course_id = myClassDto.course_id,
+            };
+
+            try
+            {
+                bool result = _myclass.CreateMyClass(myClass);
+                if (result)
+                {
+                    return StatusCode(201, myClass.class_id);
+                }
+                else
+                {
+                    return StatusCode(500, "Failed to create my class.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{class_id}")]
+        public IActionResult UpdateMyClass(Guid class_id, [FromBody] MyClassDTO myClassDto)
+        {
+            if (myClassDto == null)
+            {
+                return BadRequest("Data should be inputted.");
+            }
+
+            My_Class myClass = new My_Class
+            {
+                user_id = myClassDto.user_id,
+                course_id = myClassDto.course_id,
+            };
+
+            bool result = _myclass.UpdateMyClass(class_id, myClass);
+
+            if (result)
+            {
+                return Ok("My class updated successfully.");
+            }
+            else
+            {
+                return BadRequest("Failed to update my class.");
+            }
+        }
+
+        [HttpDelete("{class_id}")]
+        public IActionResult DeleteMyClass(Guid class_id)
+        {
+            bool result = _myclass.DeleteMyClass(class_id);
+
+            if (result)
+            {
+                return Ok("My class deleted successfully.");
+            }
+            else
+            {
+                return BadRequest("Failed to delete my class.");
+            }
+        }
+    }
+}
--- Language/Models/My_Class.cs
+++ Language/Models/My_Class.cs
@@ -1,10 +1,13 @@
-﻿namespace Language.Models
+﻿
+namespace Language.Models
 {
     public class My_Class
     {
+        internal object myclass;
+
         public Guid class_id { get; set; }
         public Guid user_id { get; set; }
         public Guid course_id { get; set; }
-
+        public List<Course> my_class { get; internal set; }
     }
 }
